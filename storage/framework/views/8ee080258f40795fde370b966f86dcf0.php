c8530013656f67c8c735729036f74c4f80dba28c
EbtihalMaher <ebtihal.maher.d@gmail.com>
2025-01-23 14:52:20 +0200

    Pixel-Positions

---
+++ storage/framework/views/8ee080258f40795fde370b966f86dcf0.php
@@ -0,0 +1,8 @@
+<?php
+    $classes = 'p-4 bg-white/5 rounded-xl border border-transparent hover:border-blue-800 group transition-colors duration-300';
+?>
+
+<div <?php echo e($attributes->merge(['class' => $classes])); ?>>
+    <?php echo e($slot); ?>
+
+</div><?php /**PATH D:\Laravel\pixel-positions\resources\views/components/panel.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/bbd4f737c0e35239e76b0ad15c7161a9.php
@@ -0,0 +1,7 @@
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-2 h-2 bg-white inline-block"></span>
+    
+    <h3 class="text-lg font-bold"><?php echo e($slot); ?></h3>
+</div>
+
+<?php /**PATH D:\Laravel\pixel-positions\resources\views/components/section-heading.blade.php ENDPATH**/ ?>
